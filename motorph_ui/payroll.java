fa562be61dc3b20a566be44bec44cb95f45ae7eb
Juliana <74715073+loanzs@users.noreply.github.com>
2024-02-25 16:31:45 +0800

    add folder for UI code

---
+++ motorph_ui/payroll.java
@@ -0,0 +1,25 @@
+
+
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class payroll {
+
+    public static void main(String[] args) {
+
+        // JFrame frame =  new JFrame();
+        // frame.setTitle("Payroll System | MotorPH");
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setResizable(true);
+        // frame.setSize(960,640);
+        // frame.setVisible(true);
+        
+        // ImageIcon image = new ImageIcon("\"C:\\Users\\juhtm\\OneDrive\\Documents\\MMDC\\s_tasks\\motorph\\imgs\\logo.jpg\""); //not working
+        // frame.setIconImage(image.getImage());
+        // frame.getContentPane().setBackground(new Color(255, 255, 255));
+        
+        ui jframe = new ui();
+        
+    }
+}
